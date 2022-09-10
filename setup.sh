b68d689a17f84519eaa6729b4a2c55f7e0f69f55
Miguel Villa Floran <49963287+Kaweees@users.noreply.github.com>
2022-09-10 10:00:55 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,3 +4,31 @@
 # Let's install some dotfiles
 # Are you ready?
 ################
+
+git pull origin main;
+
+function setupDotfiles() {
+	rsync --exclude ".git/" \
+		--exclude ".DS_Store" \
+		--exclude ".osx" \
+		--exclude "setup.sh" \
+		--exclude "README.md" \
+		--exclude "LICENSE-MIT.txt" \
+		-avh --no-perms . ~;
+	source ~/.bash_profile;
+}
+
+if [ "$1" == "--force" -o "$1" == "-f" -o "$1" == "-y" ]; then
+	setupDotfiles;
+else
+  while true; do
+    read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
+    case $yn in
+      [Yy]* )
+        setupDotfiles;
+      [Nn]* )
+        break;
+      * ) echo "Invalid input, please try again.";;
+    esac
+  done
+unset setupDotfiles;
