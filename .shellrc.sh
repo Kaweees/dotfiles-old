faf3ddf982953f3bb987bc43a4d4a536a5a2c391
Miguel Villa Floran <49963287+Kaweees@users.noreply.github.com>
2022-09-10 08:27:31 -0700

    Update .shellrc.sh

--- .shellrc.sh
+++ .shellrc.sh
@@ -1,1 +1,19 @@
+# Load ~/.zshrc or ~/.bashrc file depending on operating system.
+# Executable on macOS and Linux.
+
+####
+# Check OS
+####
+
+IS_MAC = false
+IS_LINUX = false
+
+case "$(uname -s)" in
+  Darwin*)
+    IS_MAC = true
+    ;;
+  Linux*)
+    IS_LINUX = true
+    ;;
+esac
 
