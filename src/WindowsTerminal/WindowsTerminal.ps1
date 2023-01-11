f0bf6d49743790117c3a1133fa172067d477819f
Miguel Villa Floran <49963287+Kaweees@users.noreply.github.com>
2023-01-11 08:45:08 -0800

    Update WindowsTerminal.ps1

--- src/WindowsTerminal/WindowsTerminal.ps1
+++ src/WindowsTerminal/WindowsTerminal.ps1
@@ -7,3 +7,23 @@ function Install-OhMyPosh {
   Install-Module -Name "PSWebSearch" -Repository "PSGallery";
   Install-Module -Name "PSReadLine" -Repository "PSGallery" -RequiredVersion 2.1.0;
 }
+
+function Set-OhMyPosh-Theme {
+  $DotfilesOhMyPoshThemePath = Join-Path -Path $DotfilesWorkFolder -ChildPath "WindowsTerminal" | Join-Path -ChildPath ".omp.json";
+  Write-Host "Coping Oh My Posh custom theme:" -ForegroundColor "Green";
+  Copy-Item $DotfilesOhMyPoshThemePath -Destination $env:UserProfile;
+  Write-Host "Oh My Posh theme was successfully created." -ForegroundColor "Green";
+}
+
+function Set-PowerShell-Profile {
+  $DotfilesWindowsTerminalProfilePath = Join-Path -Path $DotfilesWorkFolder -ChildPath "WindowsTerminal" | Join-Path -ChildPath "Microsoft.PowerShell_profile.ps1";
+  if (-not (Test-Path -Path $Profile)) {
+    Write-Host "Creating empty PowerShell profile:" -ForegroundColor "Green";
+    New-Item -Path $Profile -ItemType "file" -Force;
+  }
+  Write-Host "Coping PowerShell profile:" -ForegroundColor "Green";
+  Copy-Item $DotfilesWindowsTerminalProfilePath -Destination $Profile;
+  Write-Host "Activating PowerShell profile:" -ForegroundColor "Green";
+  . $Profile;
+  Write-Host "PowerShell profile was successfully created." -ForegroundColor "Green";
+}
