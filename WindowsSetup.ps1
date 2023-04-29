19a59a43949a3a31bf1a913e92187e6ad526022c
Miguel Villa Floran <49963287+Kaweees@users.noreply.github.com>
2023-04-29 01:44:18 -0700

    Windows go bye bye

--- WindowsSetup.ps1
+++
@@ -1,8 +0,0 @@
-# Let's install some dotfiles
-# Are you ready?
-
-$GitHubRepositoryUri = "https://github.com/${GitHubRepositoryAuthor}/${GitHubRepositoryName}/archive/refs/heads/main.zip";
-$DotfilesFolder = Join-Path -Path $HOME -ChildPath ".dotfiles";
-$ZipRepositoryFile = Join-Path -Path $DotfilesFolder -ChildPath "${GitHubRepositoryName}-main.zip";
-$DotfilesWorkFolder = Join-Path -Path $DotfilesFolder -ChildPath "${GitHubRepositoryName}-main" | Join-Path -ChildPath "src";
-$DownloadResult = $FALSE;
--- src/Python/Python.ps1
+++
@@ -1,7 +0,0 @@
-function Install-Python {
-  choco install -y python3
-  python --version
-  python -m pip install --upgrade pip
-}
-
-Install-Python;
